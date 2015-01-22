37a146ed95dabd1e15869d56f818e04303f6b3c1
Jackson Hoose <jacksonhoose@gmail.com>
2015-01-22 12:24:56 -0800

    add catch all route

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,14 @@
 module.exports = function applicationRouter(app) {
+
   // mount user router to /api
   app.use('/api/user', require('./api/user'));
+
+  /**
+   * catch all other routes and send back to
+   * index for angular to handle
+   */
+  app.get('/*', function(req, res, next) {
+    res.render('index');
+  });
 
 };
