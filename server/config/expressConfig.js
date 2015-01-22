7f3985cc47f5d308d45b3def1c1c6ea1c60f1a0d
Jackson Hoose <jacksonhoose@gmail.com>
2015-01-22 10:48:14 -0800

    add config file

--- server/config/expressConfig.js
+++ server/config/expressConfig.js
@@ -1,16 +1,21 @@
 var express = require('express');
-var parser = require('body-parser');
+var bodyParser = require('body-parser');
 var morgan = require('morgan');
 
 module.exports = function expressConfig (app) {
-  
+  var env = app.get('env');
+
   // standard POST request body parser
   app.use(bodyParser.urlencoded({ extended: false }));
-  
+
   // parse JSON posts
   app.use(bodyParser.json());
 
   // HTTP request logger middleware
   app.use(morgan());
+
+  if(env !== 'production'){
+    app.set('port', 3000);
+  }
 
 };
