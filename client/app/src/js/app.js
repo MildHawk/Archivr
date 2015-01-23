9f6d10248666595e3947e62785fe9eda351c6074
rwuebker <rlwuebker@gmail.com>
2015-01-22 18:17:32 -0800

    add auth controller and JWT

--- client/app/src/js/app.js
+++ client/app/src/js/app.js
@@ -5,8 +5,28 @@ function config($urlRouterProvider, $stateProvider, $locationProvider) {
 
 }
 
+function AttachTokens($window) {
+  // this is an $httpInterceptor
+  // its job is to stop all out going request
+  // then look in local storage and find the user's token
+  // then add it to the header so the server can validate the request
+  var AttachTokens = {
+    request: function (object) {
+      var jwt = $window.localStorage.getItem('com.archivr');
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return AttachTokens;
+}
+
 angular
   .module('Archivr', [
-    'ui.router'
+    'ui.router',
+    'Archivr.auth'
   ])
-  .config(config);
+  .config(config)
+  .factory('AttachTokens', AttachTokens)
---
+++ client/app/src/js/services/services.js
@@ -0,0 +1,55 @@
+
+function Auth($http, $location, $window) {
+  // Don't touch this Auth service!!!
+  // it is responsible for authenticating our user
+  // by exchanging the user's username and password
+  // for a JWT from the server
+  // that JWT is then stored in localStorage as 'com.shortly'
+  // after you signin/signup open devtools, click resources,
+  // then localStorage and you'll see your token from the server
+  var signin = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signin',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+
+
+  var signup = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signup',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+  var isAuth = function () {
+    return !!$window.localStorage.getItem('com.archivr');
+  };
+
+  var signout = function () {
+    console.log("in services... signout!!!");
+    $window.localStorage.removeItem('com.archivr');
+    $location.path('/signin');
+  };
+
+
+  return {
+    signin: signin,
+    signup: signup,
+    isAuth: isAuth,
+    signout: signout
+  };
+});
+
+
+angular.module('Archivr.services', [])
+.factory('Auth', Auth);
