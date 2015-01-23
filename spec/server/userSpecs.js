19b8249416b3db641370b194ade8b02e520a3dd5
Jackson Hoose <jacksonhoose@gmail.com>
2015-01-22 17:18:59 -0800

    fix syntax error in userSpec, add resource controllers

--- spec/server/userSpecs.js
+++ spec/server/userSpecs.js
@@ -4,11 +4,11 @@
 
 describe('User', function() {
 
-  beforeEach(function () {
+  beforeEach(function() {
   });
 
   describe('#createUser', function() {
-    it('should create a new user', function(){
+    it('should create a new user', function() {
       createUser("Ruben", "1010");
       //var user = new User({username: 'Ruben', password: '1010'});
       //user.save();
@@ -17,28 +17,28 @@ describe('User', function() {
   });
 
   describe('#createGallery', function() {
-    it('should create a new gallery for a specific user'){
+    it('should create a new gallery for a specific user', function() {
       var user = createUser("Ruben", "1010");
-      user.createGallery()
-    };
+      user.createGallery();
+    });
   });
-  
+
   describe('#getGalleries', function() {
-    it('should serve all galleries for a specific user'){
+    it('should serve all galleries for a specific user', function() {
       var user = createUser("Ruben", "1010");
       var gallery = user.createGallery();
       var gallery2 = user.createGallery();
       expect(user.getGalleries()).to.equal([gallery, gallery2]);
-    };
-  })
+    });
+  });
 
   describe('#getGallery', function() {
-    it('should serve a certain gallery from a specific user'){
+    it('should serve a certain gallery from a specific user', function() {
       var user = createUser("Ruben", "1010");
       var gallery = user.createGallery();
       var gallery2 = user.createGallery();
       expect().to.equal(gallery);
-    };
+    });
   });
 
   //describe('', function() {
