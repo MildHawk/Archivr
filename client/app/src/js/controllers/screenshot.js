30b687877700074b1100faf6d1e6bee36a72971b
Ruben V <ruben1586@gmail.com>
2015-01-30 11:20:36 -0800

    add screenshot view and service

--- client/app/src/js/controllers/screenshot.js
+++ client/app/src/js/controllers/screenshot.js
@@ -5,13 +5,21 @@
  */
 
 function ScreenshotController(Screenshot, User) {
-  this.screenshot = {};
 
+  this.screenshot = screenshot;
+
+  this.getScreenshot = function(id) {
+    Screenshot.getScreenshot(id)
+      .success(function(data) {
+        console.log(data);
+        this.screenshot = data;
+      })
+  };
 }
 ScreenshotController.$inject = ['Screenshot'];
 
 angular.module('Archivr.screenshot', [
   'Archivr.services.Screenshot',
-  'Archivr.services.Screenshot'
+  'Archivr.services.User'
 ]).controller('ScreenshotController', ScreenshotController);
 
