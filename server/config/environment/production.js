71ee377aeaa43d7da37762f797d5173736a8dfde
Andrew Smith <andrewsmith@alumni.stanford.edu>
2015-01-31 19:52:39 -0800

    using redisStore
    configure redis store for session storage
    
    clean lint
    
    debug
    
    debug
    
    debug

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -2,6 +2,11 @@
 
 // Production specific configuration
 // =================================
+
+// Parse RedisCloud environment variable
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL || '');
+
 module.exports = {
   // Server IP
   ip:       process.env.OPENSHIFT_NODEJS_IP ||
@@ -19,5 +24,12 @@ module.exports = {
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             'mongodb://localhost/archivr'
+  },
+
+  // Redis connection options
+  redis: {
+    hostname: redisURL.hostname || '127.0.0.1',
+    port: redisURL.port || 6379,
+    auth: redisURL.auth ? redisURL.auth.split(':')[1] : undefined
   }
 };
