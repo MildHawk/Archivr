365a1255a1ffa4a3a2d09fe9faf66c07dbbd3e9d
Ruben V <ruben1586@gmail.com>
2015-01-31 16:55:08 -0800

    change user_id to username

--- server/api/screenshot/screenshotModel.js
+++ server/api/screenshot/screenshotModel.js
@@ -5,7 +5,7 @@ var screenshotsSchema = mongoose.Schema({
   url: { type: String, required: true },
   originalImage: { type: String, required: true },
   annotatedImage: { type: String, required: true }, //--> annotation functionality to be implemented on the front-end, see wiki for more info about libraries suggested 
-  user_id: { type: String, required: true },
+  username: { type: String, required: true },
   visits: { type: Number, default: 0 },
   access: {type: String, default: 'public'},
   createdAt: { type: Date, default: Date.now }
