aaf3785bece8edfde9f756e1dd0d5d221d4a37b4
Ruben V <ruben1586@gmail.com>
2015-01-31 16:57:19 -0800

    add profileImage to user collection in db

--- server/api/user/userModel.js
+++ server/api/user/userModel.js
@@ -7,6 +7,7 @@ var bcrypt   = require('bcrypt-nodejs');
 var usersSchema = mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  profileImage: { type: String },
   images: { type: Array },
   galleries: { type: Array },
   createdAt: { type: Date, default: Date.now },
