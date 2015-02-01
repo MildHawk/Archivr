40806a3b57f008aa2c5960f6b752d3cd99aa9228
Ruben V <ruben1586@gmail.com>
2015-01-31 23:12:25 -0800

    fix conditional

--- server/api/screenshot/screenshotController.js
+++ server/api/screenshot/screenshotController.js
@@ -31,9 +31,9 @@ exports.list = function(req, res, next){
 exports.create = function(req, res, next) {
   var username = req.params.username;
   var url = req.body.url;
-  if( (url.parse(url).protocol !== 'http:' || 'https:') || url.parse(url).hostname.indexOf('.') === -1 ) { //regex to check if it contains http and a dot followed by something)
+  if( (url.parse(url).protocol !== 'http:' &&  url.parse(url).protocol !== 'https:')  || url.parse(url).hostname.indexOf('.') === -1 ) { //Possible cleaner conditional with regex to check if it contains http and a dot followed by something)
     res.status(500).json({ message: 'Invalid URL'});
-  }
+  })
 
   takeScreenshot(url, function(err, imageUrl, imagePublicId) {
     if (err) return res.status(500).json({ message: err });
