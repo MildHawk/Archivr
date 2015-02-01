e527716fdfb546a569c0dc574a6a7f7cd7a5623d
Ruben Vicario Gonzalez <ruben1@users.noreply.github.com>
2015-01-31 17:39:48 -0800

    Update screenshotModel.js

--- server/api/screenshot/screenshotModel.js
+++ server/api/screenshot/screenshotModel.js
@@ -4,7 +4,7 @@ var db = require('../../db/index.js');
 var screenshotsSchema = mongoose.Schema({
   url: { type: String, required: true },
   originalImage: { type: String, required: true },
-  //annotation functionality to be implemented on the front-end, see wiki for more info about libraries suggested 
+  // annotation functionality to be implemented on the front-end, see wiki for more info about libraries suggested 
   annotatedImage: { type: String, required: true }, 
   username: { type: String, required: true },
   visits: { type: Number, default: 0 },
