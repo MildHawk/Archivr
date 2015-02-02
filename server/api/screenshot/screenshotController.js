d39b7bd5a3db16c9ee0e2276a1fd7de9e275ea7b
Ruben V <ruben1586@gmail.com>
2015-02-02 14:24:50 -0800

    add return

--- server/api/screenshot/screenshotController.js
+++ server/api/screenshot/screenshotController.js
@@ -50,7 +50,7 @@ exports.create = function(req, res, next) {
 
   var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
   if( !url.match(rValidUrl) ) { 
-    res.status(500).json({ message: 'Invalid URL'});
+    return res.status(500).json({ message: 'Invalid URL'});
   }
 
   takeScreenshot(url, width, height, function(err, imageUrl, imagePublicId) {
