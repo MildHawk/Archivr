3c1011558938babe5512e33e40edaf94aa2fa3b1
Ruben V <ruben1586@gmail.com>
2015-02-02 14:11:26 -0800

    change conditional

--- server/api/screenshot/screenshotController.js
+++ server/api/screenshot/screenshotController.js
@@ -49,7 +49,7 @@ exports.create = function(req, res, next) {
   var height = req.body.height;
 
   var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
-  if( (urlModule.parse(url).protocol !== 'http:' &&  urlModule.parse(url).protocol !== 'https:') || !url.match(rValidUrl) ) { 
+  if( !url.match(rValidUrl) ) { 
     res.status(500).json({ message: 'Invalid URL'});
   }
 
