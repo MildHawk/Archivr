7da9f50e04f0680ff1eb6d874366cd3cdbf5076a
Tole <Tole>
2015-02-05 16:15:06 -0800

    can access canvas at current state in controller

--- client/app/src/js/controllers/screenshot.js
+++ client/app/src/js/controllers/screenshot.js
@@ -4,12 +4,30 @@
  * Handles display of a single screenshot.
  */
 
-function ScreenshotController(Screenshot, User, screenshot) {
+function ScreenshotController(Screenshot, User, screenshot, $window, $document) {
   this.screenshot = screenshot;
+
   this.message = 'Four developers in SF made an app. You will not believe what happened next!';
   console.log(screenshot);
+  // console.log(this.screenshot.el)
+  this.saveScreenShot = function(data, canvas){
+    console.log('Data from directive:', data);
+    // console.log(el)
+    console.log('running saveScreenshot');
+    // var canvas = $document;
+     // here is the most important part because if you dont replace you will get a DOM 18 exception.
+    var image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream'); 
+    
+
+    // it will save locally
+    console.log('should be saved');
+    // $window.location.href=image;
+    console.log('should be in local storage');
+  };
+
+
 }
-ScreenshotController.$inject = ['Screenshot', 'User', 'screenshot'];
+ScreenshotController.$inject = ['Screenshot', 'User', 'screenshot','$window','$document'];
 
 angular.module('Archivr.screenshot', [
   'Archivr.services.Screenshot',
--- client/app/src/js/directives/editableScreenshot.js
+++ client/app/src/js/directives/editableScreenshot.js
@@ -3,7 +3,8 @@ function editableScreenshot() {
   var directive = {};
 
   directive.scope = {
-    screenshot: '=screenshot'
+    screenshot: '=screenshot',
+    save: '=save'
   };
 
   // replace with template markup
@@ -18,15 +19,13 @@ function editableScreenshot() {
 
       // drawing controls
       '<div class="controls">',
-        // '<ul class="list-inline">',
-        //   '<li>',
-        //     '<a href="#" data-clear class="btn btn-default">Clear</a>',
-        //   '</li>',
-        //   '<li>',
-        //     '<a href="#" data-save class="btn btn-default">Save</a>',
-        //   '</li>',
-        // '</ul>',
+        '<ul class="list-inline">',
+          '<li>',
+            '<button data-save class="btn btn-default">Save</button>',
+          '</li>',
+        '</ul>',
       '</div>',
+
       '<canvas id="image" style="background:url(\'{{ screenshot.originalImage }}\') no-repeat left top;"',
       ' width="{{ screenshot.width }}" height="{{ screenshot.height }}"></canvas>',
       // drawing container
@@ -35,7 +34,7 @@ function editableScreenshot() {
       // ' width:{{ screenshot.width }}; height:{{ screenshot.height }};">',
       //   '<canvas id="editableScreenshot" style="position:absolute;left:0;top:0;z-index:10;"></canvas>',
       // '</div>',
-    '</div>'
+    '</div>',
   ].join('');
 
   // link
@@ -43,13 +42,21 @@ function editableScreenshot() {
     // save jquery wrapped element
     var $el = $(el);
     console.log(scope.screenshot);
-    $el.find('#image').sketch();
-
+    var canvas = $el.find('#image');
+    canvas.sketch();
+    
+    $el.find('[data-save]').on('click', function(e){
+      //var data = canvas.toDataUri();
+      console.log(e)
+      scope.save('Data from directive', canvas);
+      // scope.save()
+    });
+    // scope.save
     // enable drawing plugin and save element/api to editable
     // var editable = $el.find('#editableScreenshot').jqScribble({
     //   width: scope.screenshot.width,
     //   height: scope.screenshot.height
-    // }).data('jqScribble'); // api is stored in data
+    // })`.data('jqScribble'); // api is stored in data
 
     // $el.find('[data-clear]').on('click', function(e) {
     //   e.preventDefault();
@@ -63,7 +70,7 @@ function editableScreenshot() {
     //     console.log(imageData);
     //   });
     // });
-
+  
   };
 
   return directive;
