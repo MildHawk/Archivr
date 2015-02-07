e9aaffa0996651c0d2e5fa45dce710db2cf96981
Christine <cylee4@gmail.com>
2015-02-06 17:25:53 -0800

    Profile pic from Facebook

--- client/app/src/js/controllers/userPage.js
+++ client/app/src/js/controllers/userPage.js
@@ -8,6 +8,27 @@ function UserPageController($stateParams, User) {
   // Get User object for reference
   this.user = User.getUser();
   this.message = 'Check my Scribblr gallery!';
+  var self = this;
+  self.profileImage = '../img/anonymous_200.gif';
+  var checkLoginState = function(cb) {
+    FB.getLoginStatus(function(response) {
+      //- statusChangeCallback(response);
+      // console.log('fb userID:', response.authResponse.userID);
+      cb(response.authResponse.userID);
+    });
+  }
+
+  checkLoginState(function(userID){
+    FB.api(
+      userID + "/picture",
+      function(response) {
+        if (response && !response.error) {
+          console.log('response.data.url:', response.data.url);
+          self.profileImage = response.data.url;
+        }
+      }
+    );
+  });
 }
 
 UserPageController.$inject = ['$stateParams', 'User'];
